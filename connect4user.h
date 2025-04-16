45f182edaf194cc91db35a09cc5fadae2413366e
AidanIbrahim <aidan.ibrahim@outlook.com>
2025-04-16 11:28:23 -0400

    remove duplicate files

--- connect4user.h
+++
@@ -1,9 +0,0 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-#include <wchar.h>
-#include <locale.h>
-#include <sys/ioctl.h>
-
-void printBoard();  //Prints the board out to the user terminal
